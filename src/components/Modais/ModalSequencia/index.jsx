1d0c308f2ebab60ab11ea853bc6c02553eb27068
Leonardo Torres Rodrigues <leonardo.t.rodri@gmail.com>
2025-01-23 18:28:36 -0300

    fix: ajustando responsividade para desktop e tablet

--- src/components/Modais/ModalSequencia/index.jsx
+++ src/components/Modais/ModalSequencia/index.jsx
@@ -2,21 +2,19 @@ import trophy from '/imagens-svg/trofeus.svg'
 import styled from 'styled-components';
 
 const ModalContainer = styled.div`
+    width: 40vw;
+    height: 45vh;
     padding: 1rem;
-    border-radius: 15px;
-    background-color: ${props => props.theme['--fundo-modal']};
-`
-
-const ModalContent = styled.div`
-    width: 80vw;
-    height: auto;
     display: flex;
     flex-direction: column;
     align-items: center;
+    border-radius: 15px;
+    background-color: ${props => props.theme['--fundo-modal']};
 
     h1 {
+        height: auto;
         font-weight: 900;
-        font-size: 1.5rem;
+        font-size: 3vw;
         text-align: center;
         color: ${props => props.theme['--texto-claro']};
     }
@@ -24,11 +22,21 @@ const ModalContent = styled.div`
     strong {
         color: ${props => props.theme['--destaque']};
     }
+
+    @media (max-width: 1024px) {
+        width: 50vw;
+        height: 30vh;
+    }
+
+    @media (max-width: 847px) {
+        width: 80vw;
+    }
 `
 
 const WeekContainer = styled.div`
     width: 100%;
-    height: 100%;
+    height: 25%;
+    margin-top: 1rem;
     border-radius: 15px;
     display: flex;
     justify-content: space-evenly;
@@ -37,46 +45,44 @@ const WeekContainer = styled.div`
 `
 
 const WeekDays = styled.p`
-    width: 2.5rem;
-    height: 2.5rem;
+    width: 4vw;
+    height: 5vh;
     align-content: center;
     text-align: center;
-    font-size: 2rem;
+    font-size: 1.5vw;
     font-weight: 900;
     color: ${props => props.theme['--texto-claro']};
     background-color: ${(props) => (props.highlight ? props.theme['--destaque'] : props.theme['--fundo-modal'])};
     border-radius: 5px;
-`
 
-const ModalBottomSection = styled.div`
-    width: 100%;
-    height: 100%;
-    display: flex;
-    align-content: center;
-`
-
-const BottomSectionImg = styled.div`
-    width: 15rem;
-    height: 15rem;
-    padding: 1rem 1rem 0 1rem;
-
-    img {
-        width: 90%;
-        height: 90%;
+    @media (max-width: 1024px) {
+        height: 4vh;
+        font-size: 3vw;
     }
 `
 
-const BottomSectionText = styled.div`
-    width: 15rem;
-    height: 15rem;
+const ModalBottomContainer = styled.div`
+    width: 100%;
+    height: 50%;
+    margin-top: 1vw;
+    display: flex;
+    justify-content: center;
+    gap: 3rem;
+
+    @media (max-width: 1024px) {
+        gap: 1rem;
+    }
+`
+
+const BottomSectionWrapper = styled.div`
+    width: 100%;
+    height: 100%;
     padding: 1rem 1rem 0 1rem;
 
     p {
-        width: 100%;
-        height: 100%;
         margin: 1rem;
         font-weight: 500;
-        font-size: 1.5rem;
+        font-size: 2vw;
         flex-wrap: wrap;
         color: ${props => props.theme['--texto-claro']};
     }
@@ -85,26 +91,24 @@ const BottomSectionText = styled.div`
 export default function ModalSequencia() {
     return (
         <ModalContainer>
-            <ModalContent>
-                <h1><strong>1°</strong> dia da sequência</h1>
-                <WeekContainer>
-                    <WeekDays highlight>D</WeekDays>
-                    <WeekDays>S</WeekDays>
-                    <WeekDays>T</WeekDays>
-                    <WeekDays>Q</WeekDays>
-                    <WeekDays>Q</WeekDays>
-                    <WeekDays>S</WeekDays>
-                    <WeekDays>S</WeekDays>
-                </WeekContainer>
-                <ModalBottomSection>
-                    <BottomSectionImg>
-                        <img src={trophy} alt="troféis" />
-                    </BottomSectionImg>
-                    <BottomSectionText>
-                        <p>Mantenha sua sequência para <strong>ganhar mais recompensas</strong>!</p>
-                    </BottomSectionText>
-                </ModalBottomSection>
-            </ModalContent>
+            <h1><strong>1°</strong> dia da sequência</h1>
+            <WeekContainer>
+                <WeekDays highlight>D</WeekDays>
+                <WeekDays>S</WeekDays>
+                <WeekDays>T</WeekDays>
+                <WeekDays>Q</WeekDays>
+                <WeekDays>Q</WeekDays>
+                <WeekDays>S</WeekDays>
+                <WeekDays>S</WeekDays>
+            </WeekContainer>
+            <ModalBottomContainer>
+                <BottomSectionWrapper>
+                    <img src={trophy} alt="troféis" />
+                </BottomSectionWrapper>
+                <BottomSectionWrapper>
+                    <p>Mantenha sua sequência para <strong>ganhar mais recompensas</strong>!</p>
+                </BottomSectionWrapper>
+            </ModalBottomContainer>
         </ModalContainer>
     )
 }
